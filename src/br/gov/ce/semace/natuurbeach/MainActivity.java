4e8485e90664f03b7eb6127a163fdd2d28ac2a22
Victor Sobreira <victorcsv@gmail.com>
2013-06-04 09:18:19 -0300

    Primeiro commit

---
+++ src/br/gov/ce/semace/natuurbeach/MainActivity.java
@@ -0,0 +1,39 @@
+package br.gov.ce.semace.natuurbeach;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        WebView wv = (WebView) findViewById(R.id.webview);
+        
+        WebSettings ws = wv.getSettings();
+        ws.setJavaScriptEnabled(true);
+        
+        wv.setWebViewClient(new MyCustomWebViewClient());
+        wv.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        
+        wv.loadUrl(getResources().getString(R.string.url));
+        
+    }
+    
+    private class MyCustomWebViewClient extends WebViewClient {
+    	
+    	@Override
+    	public boolean shouldOverrideUrlLoading(WebView view, String url) {
+    		view.loadUrl(url);
+    		return true;
+    	}
+    }
+    
+}
